1ba844f34d95156a335c3fe329f1a8481eb43801
Serein-King <18721369858@163.com>
2021-08-05 20:41:57 +0800

    hhh

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-// 导入样式
+// 通用导入样式
 import './style/base.less'
+// 字体图标
+import './style/iconfont.css'
 // 导入vant
 import Vant from 'vant'
 import 'vant/lib/index.css'
+// 导入rem 适配
+import 'amfe-flexible'
 Vue.config.productionTip = false
 
 Vue.use(Vant)
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="login">
     登陆组件
+    <span class="iconfont icondianzan"></span>
   </div>
 </template>
 <script>
